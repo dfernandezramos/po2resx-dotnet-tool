ef01f981b1b62c87a3eb9218d8ace274a771883a
dfernandezramos <dfernandez@longomatch.com>
2024-07-17 18:44:06 +0200

    Move RESX generation to its own class

--- src/po2resx/Program.cs
+++ src/po2resx/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Xml.Linq;
-
-namespace Po2Resx;
+﻿namespace Po2Resx;
 
 public class Program
 {
@@ -15,38 +13,7 @@ public class Program
         string inputFilePath = args[0];
         string outputFilePath = args[1];
 
-        var translations = PoParser.Parse(inputFilePath);
-        GenerateResxFile(translations, outputFilePath);
-    }
-
-    static void GenerateResxFile(Dictionary<string, string> translations, string filePath)
-    {
-        XElement resx = new("root",
-            new XElement("resheader", new XAttribute("name", "resmimetype"),
-                new XElement("value", "text/microsoft-resx")),
-            new XElement("resheader", new XAttribute("name", "version"),
-                new XElement("value", "2.0")),
-            new XElement("resheader", new XAttribute("name", "reader"),
-                new XElement("value", "System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")),
-            new XElement("resheader", new XAttribute("name", "writer"),
-                new XElement("value", "System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"))
-        );
-        
-        foreach (var entry in translations) {
-            if (string.IsNullOrWhiteSpace (entry.Key)) {
-                continue;
-            }
-
-            XAttribute nameAttribute = new("name", entry.Key);
-            XAttribute spaceAttribute = new(XNamespace.Xml + "space", "preserve");
-            string value = string.IsNullOrWhiteSpace (entry.Value) ? entry.Key : entry.Value;
-            XElement valueElement = new("value", value);
-            XElement dataElement = new("data", nameAttribute, spaceAttribute, valueElement);
-
-            resx.Add(dataElement);
-        }
-
-        XDocument resxDoc = new(new XDeclaration("1.0", "utf-8", "yes"), resx);
-        resxDoc.Save(filePath);
+        Dictionary<string, string> translations = PoParser.Parse(inputFilePath);
+        ResxGenerator.GenerateResxFile(translations, outputFilePath);
     }
 }
---
+++ src/po2resx/ResxGenerator.cs
@@ -0,0 +1,37 @@
+using System.Xml.Linq;
+
+namespace Po2Resx;
+
+public static class ResxGenerator
+{
+	public static void GenerateResxFile(Dictionary<string, string> translations, string filePath)
+	{
+		XElement resx = new("root",
+			new XElement("resheader", new XAttribute("name", "resmimetype"),
+				new XElement("value", "text/microsoft-resx")),
+			new XElement("resheader", new XAttribute("name", "version"),
+				new XElement("value", "2.0")),
+			new XElement("resheader", new XAttribute("name", "reader"),
+				new XElement("value", "System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")),
+			new XElement("resheader", new XAttribute("name", "writer"),
+				new XElement("value", "System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"))
+		);
+
+		foreach (var entry in translations) {
+			if (string.IsNullOrWhiteSpace (entry.Key)) {
+				continue;
+			}
+
+			XAttribute nameAttribute = new("name", entry.Key);
+			XAttribute spaceAttribute = new(XNamespace.Xml + "space", "preserve");
+			string value = string.IsNullOrWhiteSpace (entry.Value) ? entry.Key : entry.Value;
+			XElement valueElement = new("value", value);
+			XElement dataElement = new("data", nameAttribute, spaceAttribute, valueElement);
+
+			resx.Add(dataElement);
+		}
+
+		XDocument resxDoc = new(new XDeclaration("1.0", "utf-8", "yes"), resx);
+		resxDoc.Save(filePath);
+	}
+}
